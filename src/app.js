ee6148aa42599a3abc52ca9e818dd28da505a053
Taketo Yoshida <tamanyan.sss@gmail.com>
2015-06-16 21:48:09 +0900

    modify static dir

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from './components/App'
 
+let path = decodeURI(window.location.pathname);
 
 function run() {
   // Render the top-level React component
@@ -12,8 +13,6 @@ function run() {
   };
   let element = React.createElement(App, props);
   React.render(element, document.getElementById('app'), () => {
-    let css = document.getElementById('css');
-    css.parentNode.removeChild(css);
   });
 }
 
