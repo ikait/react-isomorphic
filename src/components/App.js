3d28b440a1b328e78b98954309bf6f589c120148
Taketo Yoshida <tamanyan.sss@gmail.com>
2015-06-17 00:57:59 +0900

    add react-bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React, { PropTypes } from 'react'
-import {Link, RouteHandler} from 'react-router'
+import React, { PropTypes } from 'react';
+import {Link, RouteHandler} from 'react-router';
+import {ListGroup, ListGroupItem} from 'react-bootstrap';
 
 class App {
 
@@ -11,13 +12,14 @@ class App {
     return (
       <div>
         <h1><Link to="app">React server-side rendering sample</Link></h1>
-        <Link to="top" key='top'>top</Link>
-        <Link to="about" key='about'>about</Link>
+        <ListGroup>
+          <Link to="top" key='top'><ListGroupItem>top</ListGroupItem></Link>
+          <Link to="about" key='about'><ListGroupItem>about</ListGroupItem></Link>
+        </ListGroup>
         <RouteHandler {...this.props} />
       </div>
     );
   }
-
 }
 
 export default App;
--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,8 @@ import {Route, DefaultRoute} from 'react-router';
 
 let routes = (
   <Route name="app" path="/" handler={App}>
-  <Route name="about" handler={About} />
-  <DefaultRoute name="top" handler={Top} />
+    <Route name="about" handler={About} />
+    <DefaultRoute name="top" handler={Top} />
   </Route>
 )
 
